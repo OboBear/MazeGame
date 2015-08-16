af4e1a6c9315f8028f10d8436d7c18e531aad311
uuu1993 <634468460@qq.com>
2015-08-16 22:01:37 +0800

    commitString

--- shell.sh
+++ shell.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 read commitString
+echo "commitString: "commitString
 git add ./
 git commit -m commitString
 git push origin master
 
-echo "push to github success!"$commitString
+echo "push to github success!"
